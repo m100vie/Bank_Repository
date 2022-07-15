cf17d4d88b8ac55116d8b370a73afba4c24ab549
m100vie <msanvee@gmail.com>
2022-07-15 22:08:00 +0200

    Test

--- bank_application/src/main/java/com/exercice/bank_application/BankApplication.java
+++ bank_application/src/main/java/com/exercice/bank_application/BankApplication.java
@@ -1,13 +1,22 @@
 package com.exercice.bank_application;
 
+import java.util.*;
+import com.exercice.bank_application.Model;
+
+import ch.qos.logback.core.boolex.Matcher;
+
+import java.io.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
-	}
 
+		Controller controller = new Controller();
+	}
 }
+
